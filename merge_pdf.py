0547bfcdcd0ec0b196e35a1524476390597b7cc9
Mike Scarpati <mike.87@gmail.com>
2011-12-06 12:06:05 -0800

    adding license and removing some verbosity

--- merge_pdf.py
+++ merge_pdf.py
@@ -1,3 +1,27 @@
+"""
+Copyright 2011 Mike Scarpati. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are
+permitted provided that the following conditions are met:
+
+   1. Redistributions of source code must retain the above copyright notice, this list of
+      conditions and the following disclaimer.
+
+   2. Redistributions in binary form must reproduce the above copyright notice, this list
+      of conditions and the following disclaimer in the documentation and/or other materials
+      provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ''AS IS'' AND ANY EXPRESS OR 
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HODLER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
 import org.apache.pdfbox.util.PDFMergerUtility as PDFMergerUtility
 import org.apache.pdfbox.pdmodel as PDModel
 import org.apache.pdfbox.pdmodel.interactive.documentnavigation.outline as PDOutline
@@ -13,7 +37,6 @@ def sorted_nicely( l ):
     return sorted(l, key = alphanum_key)
 	
 files = sorted_nicely(glob.glob(os.getcwd()+"\\*.pdf"))
-print files
 fn = os.getcwd() + "\\combined.pdf"
 pmerger = PDFMergerUtility()
 pmerger.setDestinationFileName(fn)
@@ -28,13 +51,12 @@ for file in files:
     
 pmerger.mergeDocuments()
 doc = PDModel.PDDocument.load(fn)
-print "read " + str(doc.getNumberOfPages())
+print "read " + str(doc.getNumberOfPages()) + "pages"
 outline = PDOutline.PDDocumentOutline()
 doc.getDocumentCatalog().setDocumentOutline(outline)
 
 lastpg = -1
 cat = doc.getDocumentCatalog()
-print cat.getAllPages().size()
 for pdf in myPDFs:
     bm = PDOutline.PDOutlineItem()
     bm.setTitle(pdf[2])
